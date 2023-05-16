b52d61e5303a1cad98620cf0be441bcf79f1340f
Gabriel Felicidade <gabriel.felicidade@etec.sp.gov.br>
2023-05-16 19:15:14 -0300

    initial app commit

---
+++ src/main/kotlin/br/com/gabrielfelicidade/requestmedicines/RequestMedicinesApplication.kt
@@ -0,0 +1,11 @@
+package br.com.gabrielfelicidade.requestmedicines
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RequestMedicinesApplication
+
+fun main(args: Array<String>) {
+	runApplication<RequestMedicinesApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/gabrielfelicidade/requestmedicines/RequestMedicinesApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.gabrielfelicidade.requestmedicines
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RequestMedicinesApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
